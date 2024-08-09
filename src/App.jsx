4e8408797c7fdc6dc7503d6a4fb1b827be58c702
Ehsan <139962810+Ehsan-Rabobi@users.noreply.github.com>
2024-08-09 18:20:04 +0330

    complete banner2

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Menus from './components/Menus/Menus'
 import Banner from './components/Banners/Banner'
+import Banner2 from './components/Banners/Banner2'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Hero/>
         <Menus/>
         <Banner/>
+        <Banner2/>
       </main>
     </>
   )
--- src/components/Banners/Banner2.jsx
+++ src/components/Banners/Banner2.jsx
@@ -1,27 +1,16 @@
 import React from "react";
-import BannerPng from "../../assets/fruits-splash.png";
+import BannerPng from "../../assets/fruit-plate2.png";
 import { motion } from "framer-motion";
 import { FadeUp } from "../../utility/animation";
 
-const Banner = () => {
+const Banner2 = () => {
   return (
     <>
-      <section className="bg-secondary/10">
+      <section className="">
         <div
           className="container grid grid-cols-1
-        md:grid-cols-2 space-y-6 md:space-y-0 py-14"
+        md:grid-cols-2 space-y-6 md:space-y-0 py-14 md:py-24"
         >
-          <div className="flex justify-center items-center">
-            <motion.img
-            initial={{opacity:0 , scale:0.5}}
-            whileInView={{opacity:1 , scale:1}}
-            transition={{type:"spring" , stiffness:100 , delay:0.2}}
-              src={BannerPng}
-              className="w-[300px] md:max-w-[400px]
-                h-full object-cover drop-shadow"
-              alt=""
-            />
-          </div>
           <div className="flex flex-col justify-center">
             <div
               className="text-center md:text-left space-y-4
@@ -67,10 +56,22 @@ const Banner = () => {
               </motion.div>
             </div>
           </div>
+          <div className="flex justify-center items-center">
+            <motion.img
+              initial={{ opacity: 0, x: 200, rotate: 75 }}
+              whileInView={{ opacity: 1, x: 0, rotate: 0 }}
+              transition={{ duration: 1, delay: 0.2 }}
+              viewport={{ once: true }}
+              src={BannerPng}
+              className="w-[300px] md:max-w-[400px]
+                h-full object-cover drop-shadow"
+              alt=""
+            />
+          </div>
         </div>
       </section>
     </>
   );
 };
 
-export default Banner;
+export default Banner2;
